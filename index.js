b0515a91d9455f6f11e0e7bfc03d2e3c3b813e4c
Ben Wilcox <reccanti@gmail.com>
2016-10-04 23:46:45 -0400

    Render web page
    Renders a web page using pug for the moment

--- index.js
+++ index.js
@@ -21,14 +21,17 @@
 const db = require('monk')('localhost/reccantiblog');
 const app = require('koa')();
 const router = require('koa-router')();
+const views = require('koa-render');
 
+// define routes
 router
     .get('home', '/', function *(next) {
-        this.body = "welcome home";
+        this.body = yield this.render('index');
     })
     .get('posts', '/posts', function *(next) {
         this.body = "posts";
     });
 
+app.use(views('./views', 'pug'));
 app.use(router.routes());
 app.listen(3000);
